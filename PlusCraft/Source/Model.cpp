e5cd05267b20d3313d13a455e4df3dffd442fa65
Jim Drake <jimdrake55x@gmail.com>
2018-01-02 15:22:59 -0600

    Video 5: Converted Vertex Buffer Objects to Arrays so we can draw multiple objects at the same time

--- PlusCraft/Source/Model.cpp
+++ PlusCraft/Source/Model.cpp
@@ -1,43 +1,54 @@
 #include "Model.h"
 #include <GL/glew.h>
 
-
 Model::Model(const std::vector<GLfloat>& vertexPositions)
 {
-    glGenBuffers(1, &m_vertexId); // We now have the buffer id
-    glBindBuffer(GL_ARRAY_BUFFER, m_vertexId); //TODO check this out seems fishy
+    glGenVertexArrays(1, &m_vao);
+    glBindVertexArray(m_vao);
 
-    glBufferData(GL_ARRAY_BUFFER,
-                vertexPositions.size() * sizeof(vertexPositions[0]),
-                vertexPositions.data(),
-                GL_STATIC_DRAW);
+    addVBO(2,vertexPositions);
 
-
-    glVertexAttribPointer(0,
-                          2,
-                          GL_FLOAT,
-                          GL_FALSE,
-                          0,
-                          (GLvoid*) 0);
-
-    glEnableVertexAttribArray(0);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0); //TODO check this out seems fishy
-
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER,0);
 
 }
 
 Model::~Model()
 {
-    glDeleteBuffers(1,&m_vertexId);
+    glDeleteVertexArrays(1,&m_vao);
+    glDeleteBuffers(m_buffers.size(), m_buffers.data());
+
 }
+
+void Model::addVBO(int dim, const std::vector<GLfloat>& data)
+{
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER,
+                 data.size() * sizeof(data[0]),
+                 data.data(),
+                 GL_STATIC_DRAW);
+
+    glVertexAttribPointer(m_vboCount,
+                          dim,
+                          GL_FLOAT,
+                          GL_FALSE,
+                          0,
+                          (GLvoid*) 0);
+
+    glEnableVertexAttribArray(m_vboCount++);
+
+    m_buffers.push_back(vbo);
+}
+
 
 void Model::bind()
 {
-    glBindBuffer(GL_ARRAY_BUFFER, m_vertexId); //TODO check this out seems fishy
+    glBindVertexArray(m_vao);
 }
 
 void Model::unbind()
 {
-    glBindBuffer(GL_ARRAY_BUFFER, 0); //TODO check this out seems fishy
+    glBindVertexArray(0);
 }
--- PlusCraft/Source/Model.h
+++ PlusCraft/Source/Model.h
@@ -12,7 +12,15 @@ class Model
         void unbind();
 
     private:
-        GLuint m_vertexId;
+        //dim = dimensions per vertex
+        void addVBO(int dim, const std::vector<GLfloat>& data); //This method will add to our Vertex Array Object
+
+        //Our VAO of VBO's. Or Vertex array object containing Vertex Buffer objects.
+        std::vector<GLuint> m_buffers;
+        GLuint m_vao = 0;
+
+        //Helper Variable to keep track of VBO's in our VAO.
+        GLuint m_vboCount = 0;
 };
 
 #endif // MODEL_H_INCLUDED
